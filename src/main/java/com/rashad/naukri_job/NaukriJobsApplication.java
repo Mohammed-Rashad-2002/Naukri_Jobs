ebcab36781f61a74783bc0c181c2bb2897d0ae93
Mohammed Rashad <smohammedrashad3@gmail.com>
2023-08-20 23:33:45 +0530

    first commit

---
+++ src/main/java/com/rashad/naukri_job/NaukriJobsApplication.java
@@ -0,0 +1,13 @@
+package com.rashad.naukri_job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaukriJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaukriJobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rashad/naukri_job/NaukriJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rashad.naukri_job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaukriJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
